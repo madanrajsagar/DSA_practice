73de548946e53b7c5d1f3125be2cb8fffd952f80
Madanraj Sagar <madanrajsagar83@gmail.com>
2026-01-30 23:42:23 +0530

    30-01-26 UP

--- MaxwaterStore.java
+++ MaxwaterStore.java
@@ -1,7 +1,49 @@
 import java.util.ArrayList;
 
-// Brute-force approach
+
 public class MaxwaterStore{
+
+    // Brute-force approach
+    public static void storeWater(ArrayList<Integer> height) {
+        int maxWater = Integer.MIN_VALUE;
+
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+                int width = j - i;
+                int ht = Math.min(height.get(i), height.get(j));
+                int currWater = width * ht;
+                maxWater = Math.max(maxWater, currWater);
+
+            }
+        }
+
+        System.out.println("Maximum water it contains : " + maxWater);
+
+    }
+
+      //2 Pointer Approach O(n)
+    public static void storeWaterO(ArrayList<Integer> height) {
+        int maxWater = Integer.MIN_VALUE;
+        int fp=0;
+        int lp=height.size()-1;
+
+        while(fp<lp){
+            int wd= lp-fp;
+            int ht=Math.min(height.get(lp),height.get(fp));
+            int currWt=wd*ht;
+            maxWater= Math.max(maxWater,currWt);
+
+            if(height.get(fp)<height.get(lp)){
+                fp++;
+            }else{
+                lp--;
+            }
+        }
+
+        System.out.println("Maximum water it contains : " + maxWater);
+
+    }
+  
     public static void main(String args[]){
         ArrayList<Integer> height= new ArrayList<>();
         // 1,8,6,2,5,4,8,3,7
@@ -15,19 +57,7 @@ public class MaxwaterStore{
         height.add(3);
         height.add(7);
         
-    int maxWater= Integer.MIN_VALUE;
-
-    for(int i=0;i<height.size();i++){
-        for(int j=i+1;j<height.size();j++){
-            int width= j-i;
-            int ht= Math.min(height.get(i),height.get(j));
-            int currWater= width*ht;
-            maxWater= Math.max(maxWater,currWater);
-
-        }
-    }
-
-    System.out.println("Maximum water it contains : "+maxWater);
-
+        storeWater(height);
+        storeWaterO(height);
     }
 }
