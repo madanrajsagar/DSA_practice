9528ab8291f923133ffa622ce327542c2405c418
Madanraj Sagar <madanrajsagar83@gmail.com>
2026-03-13 22:54:58 +0530

    13-03-2026

--- ActivitySelectionProblem.java
+++ ActivitySelectionProblem.java
@@ -1,45 +1,35 @@
 import java.util.*;
-
 public class ActivitySelectionProblem{
     public static void main(String args[]){
-        ArrayList<Integer> ans = new ArrayList<>();
-        int startTime[]={0,1,3,5,5,8};
-        int endTime[]={6,2,4,7,9,9};
+       int startTime[]= {1,3,0,5,8,5};
+       int endTime[]={2,4,6,7,9,9};
 
-        // sorting
+        ArrayList<Integer> activities = new ArrayList<>();
+       int Act[][]= new int[endTime.length][3];
 
-        int activities[][]= new int[startTime.length][3];
-        for(int i=0;i<startTime.length;i++){
-            activities[i][0]=i;
-            activities[i][1]=startTime[i];
-            activities[i][2]=endTime[i];
+        // sorting of the endtime
+        for(int i=0;i<endTime.length;i++){
+            Act[i][0]=i+1;
+            Act[i][1]=startTime[i];
+            Act[i][2]=endTime[i];
         }
-        // lambda function ->shortform
-        Arrays.sort(activities,Comparator.comparingDouble(o -> o[2]));
-        // for(int i=0;i<activities.length;i++){
-        //     for(int j=0;j<3;j++){
-        //         System.out.print(activities[i][j]+" ");
-        //     }
-        //         System.out.println();
-            
-        // }
 
-        ans.add(0);
-        int maxAct=1;
-        int endAct=activities[0][2];
+        // lamda function ->shortform
+        Arrays.sort(Act,Comparator.comparingDouble(o->o[2]));
+        // select first activity
+        activities.add(0);
+        int maxact=1;
+        int lastend= Act[0][2];
 
         for(int i=1;i<endTime.length;i++){
-            if(activities[i][1]>=endAct){
-                ans.add(i);
-                maxAct++;
-                endAct=activities[i][2];
+            if(lastend<=Act[i][1]){
+                activities.add(i);
+                maxact++;
+                lastend=Act[i][2];
             }
         }
 
-        for(int i=0;i<maxAct;i++){
-        System.out.println("A"+ans.get(i).toString()+" ");    
-                }
-        System.out.println("Maximum number of activities we can perform : "+maxAct);    
+System.out.println("Number of activities can be performed : "+maxact);
         
 
     }
